4504a237ba696564b9abe8c3f8e4ff059c4c15d1
Samer Nassim <74414236+samer411@users.noreply.github.com>
2022-12-24 09:36:45 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,64 @@
-let arr = [];
-let before = document.getElementById("before");
-let result = document.getElementById("result");
-function getArr() {
-  let arr = [];
-  let input = document.getElementsByName("array[]");
-  for (var i = 0; i < input.length; i++) {
-    arr.push(Number(input[i].value));
-    document.createElement("p");
+function kruskal(vertices, edges) {
+  // Sort the edges in ascending order by weight
+  edges.sort((a, b) => a.weight - b.weight);
+
+  // Create a disjoint-set data structure to store the sets of vertices
+  const sets = [];
+  for (let i = 0; i < vertices.length; i++) {
+    sets.push({ parent: i, rank: 0 });
   }
-  before.innerHTML = "Before : [" + arr + "]";
-  insertionSort(arr, 8);
-  result.innerHTML = "After  : [" + arr + "]";
+
+  // Initialize the list of edges in the minimum spanning tree
+  const mst = [];
+
+  // Iterate through the edges
+  for (let i = 0; i < edges.length; i++) {
+    // Find the sets that the vertices belong to
+    const x = findSet(sets, edges[i].source);
+    const y = findSet(sets, edges[i].target);
+
+    // If the vertices are not in the same set, add the edge to the minimum spanning tree
+    // and union the sets
+    if (x !== y) {
+      mst.push(edges[i]);
+      unionSet(sets, x, y);
+    }
+  }
+
+  return mst;
 }
 
-function insertionSort(items) {
-  let len = items.length, //number of the items in the array
-    value, //the value currently being compated
-    i, //index into unsorted section
-    j; //index into sorted section
-
-  for (i = 0; i < len; i++) {
-    value = items[i];
-
-    console.log("key" + value);
-    console.log("outer" + i);
-    for (j = i - 1; j > -1 && items[j] > value; j--) {
-      items[j + 1] = items[j];
-      let p = document.createElement("p");
-      let text = document.createTextNode(
-        `Array = ${items} || key = ${value} || i = ${i} || j = ${j}`
-      );
-      p.appendChild(text);
-      before.appendChild(p);
-    }
-    items[j + 1] = value;
+// Find the set that a given vertex belongs to
+function findSet(sets, vertex) {
+  if (sets[vertex].parent !== vertex) {
+    sets[vertex].parent = findSet(sets, sets[vertex].parent);
   }
-  return items;
+  return sets[vertex].parent;
+}
+
+// Union two sets by rank
+function unionSet(sets, x, y) {
+  if (sets[x].rank > sets[y].rank) {
+    sets[y].parent = x;
+  } else {
+    sets[x].parent = y;
+    if (sets[x].rank === sets[y].rank) {
+      sets[y].rank++;
+    }
+  }
+}
+
+function calculateMST() {
+  // Parse the input values
+  const vertices = JSON.parse(document.getElementById("vertices").value);
+  const edges = JSON.parse(document.getElementById("edges").value);
+
+  // Calculate the minimum spanning tree
+  const mst = kruskal(vertices, edges);
+
+  // Output the result
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "Minimum spanning tree: " + JSON.stringify(mst);
+
+  return false;
 }
